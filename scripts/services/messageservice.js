6c685fefcb01011bdc9a4907d2b809724babbf83
Kenny Howard <ken.chad.howard@rbs.com>
2015-02-05 21:20:09 -0700

    updates

--- scripts/services/messageservice.js
+++ scripts/services/messageservice.js
@@ -1,7 +1,20 @@
 'use strict';
 
 angular.module('chattyApp')
-  .factory('MessageService', function MessageService() {
-    return {
-    }
+  .factory('MessageService', function MessageService($http) { // remember to add the $http
+  	this.getMessages = function() {
+  		return $http({
+  			method: 'GET',
+  			url: 'http://localhost:11000'
+  		})
+  	}  		
+  	this.sendMessages = function(msg) {
+  		var data = {"message": msg}
+  		return $http({
+  			method: 'POST',
+  			url: 'http://localhost:11000',
+  			data: data
+  		})
+  	} 
+    
   });
